e06145ccc475811f72165e7a3598a4747dd4e114
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-01-11 16:57:59 -0700

    Passed Tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2 
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 4
+let num4 = 8
+let mod = num3 % num4
+
+let max = Math.max(1, 2, 3, 4, 20);
